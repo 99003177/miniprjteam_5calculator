6eb19e316fdd801f7e08f9dfa434689703402224
99003177 <gundeti.chandramounika@ltts.com>
2020-12-06 19:05:38 +0530

    src

--- implementation/inc/calculator_operations.h
+++ implementation/inc/calculator_operations.h
@@ -5,8 +5,6 @@
 */
 #ifndef __CALCULATOR_OPERATIONS_H__
 #define __CALCULATOR_OPERATIONS_H__
-//#ifndef __CALCULATOR_OPERATIONS_H_
-//#define__CALCULATOR_OPERATIONS_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,7 +26,6 @@ int add(int operand1, int operand2);
 * @param[in] operand2
 * @return Result of operand1 - operand2
 */
-//int subtract(int operand1, int operand2);
 int subtract(int operand1,int operand2);
 
 /**
@@ -37,8 +34,7 @@ int subtract(int operand1,int operand2);
 * @param[in] operand2
 * @return Result of operand1 * operand2
 */
-//int multiply(int operand1, int operand2);
-int multiply(int operand1,int operand2);
+int multiply(int operand1, int operand2);
 
 /**
 * divides the operand1 by operand2 and returns the result 
@@ -47,7 +43,7 @@ int multiply(int operand1,int operand2);
 * @return integer value of the operand1 / operand2
 * @note returns 0 for divide by 0 error
 */
-int divided(int operand1, int operand2);
+int divide(int operand1, int operand2);
 
 
 /**
