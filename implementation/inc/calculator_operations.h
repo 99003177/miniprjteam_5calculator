7d887b0ea0e879cce22475a538ba3e0d08642d36
99003175 <75428707+99003175@users.noreply.github.com>
2020-12-06 15:33:13 +0530

    Update calculator_operations.h

--- implementation/inc/calculator_operations.h
+++ implementation/inc/calculator_operations.h
@@ -42,7 +42,7 @@ int multiply(int operand1,int operand2);
 
 /**
 * divides the operand1 by operand2 and returns the result 
-* @param[in] operand1 
+* @param[in] operand2
 * @param[in] operand2 
 * @return integer value of the operand1 / operand2
 * @note returns 0 for divide by 0 error
